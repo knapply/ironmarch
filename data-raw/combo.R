317d5cfb43bd21b4e00afc1985d9cb3669486c23
Brendan Knapp <brendan.knapp@nps.edu>
2020-04-04 11:42:25 -0700

    combo_*_df

--- data-raw/combo.R
+++ data-raw/combo.R
@@ -1,6 +1,8 @@
 assignInNamespace("print.data.table", function(x, ...) print(tibble::as_tibble(x), ...), ns = "data.table")
 
 library(data.table)
+print.data.table <- function(x) print(tibble::as_tibble(x))
+print.data.frame <- function(x) print(tibble::as_tibble(x))
 
 orig_msgs <- copy(ironmarch::im_orig_dfs$orig_message_posts)
 core_msgs <- copy(ironmarch::im_core_dfs$core_message_posts)
@@ -26,6 +28,8 @@ combo_messages_df <- merge(
 
 stopifnot(nrow(combo_messages_df) == 22309)
 
+setDT(combo_messages_df)
+
 combo_messages_df[
   , msg_post := fifelse(is.na(core_msg_post), orig_msg_post, core_msg_post)
   ][, msg_author_id := fifelse(is.na(core_msg_author_id), 
@@ -43,7 +47,9 @@ combo_messages_df[
 stopifnot(nrow(combo_messages_df[is.na(msg_author_id)]) == 0L)
 stopifnot(nrow(combo_messages_df[is.na(msg_post)]) == 0L)
 
-# usethis::use_data(combo_messages_df, overwrite = TRUE)
+combo_messages_df <- tibble::as_tibble(combo_messages_df)
+
+usethis::use_data(combo_messages_df, overwrite = TRUE)
 
 target_cols <- c(
   "member_id", "name", "member_group_id", "email", "joined", 
@@ -54,11 +60,14 @@ target_cols <- c(
   "members_cache", "members_disable_pm", "failed_logins", "members_profile_views"
 )
 
-orig_members <- copy(ironmarch::im_orig_dfs$orig_members
+im_orig_dfs_orig_members <- setDT(copy(ironmarch::im_orig_dfs$orig_members))
+
+orig_members <- copy(im_orig_dfs_orig_members
                      )[, ..target_cols
                        ][, priority := 2L
                          ]
-core_members <- copy(ironmarch::im_core_dfs$core_members
+im_core_dfs_core_members <- setDT(copy(ironmarch::im_core_dfs$core_members))
+core_members <- copy(im_core_dfs_core_members
                      )[, ..target_cols
                        ][, priority := 1L
                          ]
@@ -102,6 +111,7 @@ combo_members_df <- merge(
               )[!is.na(address)]
         })]
 
+combo_members_df <- tibble::as_tibble(combo_members_df)
 
 usethis::use_data(combo_members_df, overwrite = TRUE)
 
